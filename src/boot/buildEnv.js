7cdc7dad3d9cb808073bca0d48b495a0531bdb2e
hbrls <shuaizhexu@gmail.com>
2025-09-28 16:05:55 +0800

    !: rum-miniapp 2.2.8

--- src/boot/buildEnv.js
+++ src/boot/buildEnv.js
@@ -1,4 +1,4 @@
 export var buildEnv = {
-  sdkVersion: '2.2.7',
+  sdkVersion: '2.2.8',
   sdkName: 'df_miniapp_rum_sdk'
 };
--- src/core/transport.js
+++ src/core/transport.js
@@ -22,12 +22,13 @@ httpRequest.prototype = {
     request({
       method: "POST",
       header: {
-        "content-type": "text/plain;charset=UTF-8"
-        // "x-client-timestamp": new Date().getTime().toString(),
+        "content-type": "text/plain;charset=UTF-8",
+        "x-client-timestamp": new Date().getTime().toString()
       },
       headers: {
-        "content-type": "text/plain;charset=UTF-8" // 兼容其他
-        // "x-client-timestamp": new Date().getTime().toString(),
+        "content-type": "text/plain;charset=UTF-8",
+        // 兼容其他
+        "x-client-timestamp": new Date().getTime().toString()
       },
       url,
       data
